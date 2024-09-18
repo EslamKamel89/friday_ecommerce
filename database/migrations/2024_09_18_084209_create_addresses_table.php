7ce1914bfff9dcde859b4d9a4102c7ef756f33d6
EslamKamel89 <eslam_kamel_89@hotmail.com>
2024-09-18 12:52:09 +0300

    creating and definng the many to many relationship between orders and products. creating the Pivot model OrderProduct

--- database/migrations/2024_09_18_084209_create_addresses_table.php
+++ database/migrations/2024_09_18_084209_create_addresses_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('addresses', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create( 'addresses', function (Blueprint $table) {
+			$table->id();
+			// $table->
+			$table->timestamps();
+		} );
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('addresses');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists( 'addresses' );
+	}
 };
