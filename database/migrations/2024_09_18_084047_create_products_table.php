d04db5e587c8ad24dafea46a23316890f508f7e0
EslamKamel89 <eslam_kamel_89@hotmail.com>
2024-09-18 15:37:05 +0300

    install filament 3

--- database/migrations/2024_09_18_084047_create_products_table.php
+++ database/migrations/2024_09_18_084047_create_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
 	public function up(): void {
 		Schema::create( 'products', function (Blueprint $table) {
 			$table->id();
-			$table->foreignId( 'cateogry_id' )->constrained()->cascadeOnDelete();
+			$table->foreignId( 'category_id' )->constrained()->cascadeOnDelete();
 			$table->foreignId( 'brand_id' )->constrained()->cascadeOnDelete();
 			$table->string( 'name' );
 			$table->string( 'slug' )->unique();
