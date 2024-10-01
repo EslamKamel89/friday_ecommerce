2963551cdf98e47b81cffd1a1d38fc8819cdc0dc
EslamKamel89 <eslam_kamel_89@hotmail.com>
2024-10-01 17:19:30 +0300

    making register page dymaic

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
-class RegisterPage extends Component
-{
-    public function render()
-    {
-        return view('livewire.auth.register-page');
-    }
+
+
+#[Title('Register-Friday') ]
+class RegisterPage extends Component {
+	public $name;
+	public $email;
+	public $password;
+
+	public function render() {
+		return view( 'livewire.auth.register-page' );
+	}
+	public function save() {
+		$this->validate( [ 
+			'name' => 'required|min:3|max:255',
+			'email' => 'required|email|min:3|unique:users,email|max:255',
+			'password' => 'required|min:3|max:255',
+		] );
+		// info( 'save', [ $this->name, $this->email, $this->password ] );
+		$user = User::create( [ 
+			'name' => $this->name,
+			'email' => $this->email,
+			'password' => $this->password,
+		] );
+		auth()->login( $user, true );
+		return redirect()->intended();
+	}
 }
